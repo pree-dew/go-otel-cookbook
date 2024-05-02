1ff9d810b51b8ce2d70d5d36fd13a04dbe815c1c
Saurabh Hirani <saurabh.hirani@gmail.com>
2024-05-02 16:30:04 +0530

    adding env vars for each section

---
+++ push-using-agent/otel-agent/env.sh
@@ -0,0 +1,2 @@
+export ENDPOINT_DOMAIN='otel-collector:4317'
+export REMOTE_WRITE_URL='http://victoriametrics:8429/api/v1/write'
---
+++ push-using-direct-remotewrite/via-collector/env.sh
@@ -0,0 +1,2 @@
+export ENDPOINT_DOMAIN='otel-agent:4317'
+export REMOTE_WRITE_URL='http://victoriametrics:8429/api/v1/write'
